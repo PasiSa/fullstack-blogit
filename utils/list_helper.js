981d1e4b9942042bd727cb0a94c6231a7edb764b
Pasi Sarolahti <pasi.sarolahti@iki.fi>
2021-01-02 23:42:10 +0200

    Task 4.5 done.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce((prev, cur) => prev + cur.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const res = blogs.reduce((prev, cur) => {
+    if (!prev || cur.likes > prev.likes) {
+      return cur
+    } else {
+      return prev
+    }
+  }, null)
+  return res
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
