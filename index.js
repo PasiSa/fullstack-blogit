2a477d90f8a2646a6ef9a8cdcbae47f40865940d
Pasi Sarolahti <pasi.sarolahti@iki.fi>
2021-01-02 08:39:56 +0200

    Fixing Task 4.2.

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+//const http = require('http')
+const app = require('./app')
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
